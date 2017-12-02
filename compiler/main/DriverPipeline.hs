37ad141f8c516921ba9e5c59bb1beb050b0bfc41
Will Fancher <elvishjerricco@gmail.com>
2017-12-02 16:58:59 -0500

    Begin creating a proper ArchWasm

--- compiler/main/DriverPipeline.hs
+++ compiler/main/DriverPipeline.hs
@@ -1251,7 +1251,7 @@ runPhase (RealPhase cc_phase) input_fn dflags
 
         -- Decide next phase
         next_phase <-
-            if platformArch platform == ArchUnknown
+            if platformArch platform == ArchWasm
               then maybeMergeForeign
               else return (As False)
 
@@ -1326,7 +1326,7 @@ runPhase (RealPhase cc_phase) input_fn dflags
 
                        -- We should not make assumptions about assembly languages
                        -- that we do not know about
-                       ++ (if platformArch platform == ArchUnknown
+                       ++ (if platformArch platform == ArchWasm
                              then ["-c"]
                              else [ "-S"])
                        ++ cc_opt
